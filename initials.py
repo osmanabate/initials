49df7b9012360e0d406284dbd0a1a720507cf0f9
Osman Abate <osmanabate@Osmans-MacBook-Air.local>
2017-05-01 21:52:27 -0700

    First commit

---
+++ initials.py
@@ -0,0 +1,24 @@
+
+
+#__author__: "osman abate"
+#__version__: "1.0"
+#date: 04/27/2017
+#assignment: get_initials
+#description: Given a users full name, return capitalized initials
+#testcases
+
+def get_initials(fullname):
+    name = (fullname)
+    name_list = name.split()
+
+    initials = ""
+
+    for name in name_list:
+        initials += name[0].upper()
+    return initials
+
+
+def main():
+    print(get_initials(input('enter your name: \n\t')))
+if __name__ == '__main__':
+    main()
